72333ccd5cfb9c1dc8297dbd9751dbd6f8d3c681
Anton Ermolenko <aner@fastmail.com>
2019-05-07 13:20:53 +1000

    Added mpd config

--- stow.sh
+++ stow.sh
@@ -2,10 +2,11 @@
 
 mkdir -p "${HOME}/.config"
 mkdir -p "${HOME}/.local/bin"
+mkdir -p "${HOME}/.cache/mpd"
 
 stow -R bash -t "${HOME}"
 stow -R nvim -t "${HOME}"
-stow -R hosts -t "${HOME}"
+stow -R mpd -t "${HOME}"
 stow -R scripts -t "${HOME}/.local/bin"
 stow -R hammerspoon -t "${HOME}"
 
