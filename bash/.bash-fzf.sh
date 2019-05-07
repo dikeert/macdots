13a924b29e0ff1fef906d976564100d0cc1a0e4b
Anton Ermolenko <aner@fastmail.com>
2019-05-07 13:22:09 +1000

    Better formatting, itunes replaced with Cantanta and option to open code with fzf

--- bash/.bash-fzf.sh
+++ bash/.bash-fzf.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
+dirf() {
+	local dir="$1"
+
+	find "${HOME}/${dir}" -type d -maxdepth 1 -mindepth 1 | xargs -I {} basename {} | sort -r | fzf --height=40% --sync
+}
+
 cdf() {
 	local dir="$1"
-	cd "${HOME}/${dir}/$(find "${HOME}/${dir}" -type d -maxdepth 1 -mindepth 1 | xargs -I {} basename {} | sort -r | fzf --height=40% --sync)"
+	cd "${HOME}/${dir}/$(dirf "$dir")"
+}
+
+codef() {
+	local dir="$1"
+	local target="$(dirf "$dir")"
+
+	if [ "$target" == "" ] || [ "$target" == "." ]; then
+		exit 1
+	else
+		code "${HOME}/${dir}/${target}"
+	fi
 }
 
 alias src="cdf work/src"
 alias dos="cdf work/dos"
+alias cde="codef work/src"
 
--- hammerspoon/.hammerspoon/config.lua
+++ hammerspoon/.hammerspoon/config.lua
@@ -1,270 +1,294 @@
 return {
-    windows = {
-        tolerance = 10,
-        layout = {
-            default = {
-                vertical = {
-                    center = 0.5,
-                    length = 0.644
-                },
-                horizontal = {
-                    center = 0.5,
-                    length = 0.5
-                },
-								left = {
-									position = 0,
-									length = 0.2267
-								},
-								right = {
-									position = 0,
-									length = 0.2267
-								},
-								grow = {
-									step = 0.05
-								}
-            },
-            screens = {
-                {
-                    screen = 'Color LCD',
-                    vertical = {
-                        center = 0.5,
-                        length = 0.9
-                    },
-                    horizontal = {
-                        center = 0.5,
-                        length = 0.7
-                    },
-										left = {
-											position = 0,
-											length = 0.2267
-										},
-										right = {
-											position = 0,
-											length = 0.1
-										},
-										grow = {
-											step = 0.05
-										},
-                },
-                {
-                    screen = 'DELL P2715Q',
-                    vertical = {
-                        center = 0.5,
-                        length = 0.644
-                    },
-                    horizontal = {
-                        center = 0.509,
-                        length = 0.692
-                    },
-                    left = {
-                        position = 0,
-                        length = 0.152
-                    },
-										right = {
-											position = 0,
-											length = 0.1
-										},
-                    grow = {
-                        step = 0.05
-                    }
-                }
-            },
-						apps = {
-							{
-								app = 'Telegram',
-								screen = 'Color LCD',
-								horizontal = {
-									center = 0.5,
-									length = 0.45
-								},
-								vertical = {
-									center = 0.5,
-									length = 0.644
-								},
-							},
-							{
-								app = 'Telegram',
-								screen = 'DELL P2715Q',
-								horizontal = {
-									center = 0.5,
-									length = 0.35
-								},
-								vertical = {
-									center = 0.5,
-									length = 0.55
-								},
-							},
-							{
-								app = 'Messages',
-								screen = 'Color LCD',
-								horizontal = {
-									center= 0.5,
-									length = 0.45
-								},
-								vertical = {
-									center = 0.5,
-									length = 0.644
-								},
-							},
-							{
-								app = 'Finder',
-								screen = 'Color LCD',
-								horizontal = {
-									center = 0.5,
-									length = 0.45
-								},
-								vertical = {
-									center = 0.5,
-									length = 0.644
-								}
-							},
-						}
+  windows = {
+    tolerance = 10,
+    layout = {
+      default = {
+        vertical = {
+          center = 0.5,
+          length = 0.644
+        },
+        horizontal = {
+          center = 0.5,
+          length = 0.5
+        },
+        left = {
+          position = 0,
+          length = 0.2267
+        },
+        right = {
+          position = 0,
+          length = 0.2267
+        },
+        grow = {
+          step = 0.05
         }
-    },
-    keys = {
+      },
+      screens = {
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt', 'ctrl'},
-                key = 'k'
-            },
-            cmd = 'wnd.tall',
-            args = {}
+          screen = "Color LCD",
+          vertical = {
+            center = 0.5,
+            length = 0.9
+          },
+          horizontal = {
+            center = 0.5,
+            length = 0.7
+          },
+          left = {
+            position = 0,
+            length = 0.2267
+          },
+          right = {
+            position = 0,
+            length = 0.1
+          },
+          grow = {
+            step = 0.05
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt', 'ctrl'},
-                key = 'j'
-            },
-            cmd = 'wnd.cntr',
-            args = {}
+          screen = "DELL P2715Q",
+          vertical = {
+            center = 0.5,
+            length = 0.644
+          },
+          horizontal = {
+            center = 0.509,
+            length = 0.692
+          },
+          left = {
+            position = 0,
+            length = 0.152
+          },
+          right = {
+            position = 0,
+            length = 0.1
+          },
+          grow = {
+            step = 0.05
+          }
+        }
+      },
+      apps = {
+        {
+          app = "Telegram",
+          screen = "Color LCD",
+          horizontal = {
+            center = 0.5,
+            length = 0.45
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.644
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt', 'ctrl'},
-                key = 'h'
-            },
-            cmd = 'wnd.left',
-            args = {}
-        },
-				{
-					shortcut = {
-						modifiers = {'cmd', 'alt', 'ctrl'},
-						key = 'l'
-					},
-					cmd = 'wnd.right',
-					args = {}
-				},
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt', 'ctrl'},
-                key = 'f'
-            },
-            cmd = 'wnd.growh',
-            args = {}
+          app = "Telegram",
+          screen = "DELL P2715Q",
+          horizontal = {
+            center = 0.5,
+            length = 0.35
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.55
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt', 'ctrl'},
-                key = 'b'
-            },
-            cmd = 'wnd.shrinkh',
-            args = {}
+          app = "Cantata",
+          screen = "Color LCD",
+          horizontal = {
+            center = 0.5,
+            length = 0.45
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.644
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'v'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Visual Studio Code'
+          app = "Cantata",
+          screen = "DELL P2715Q",
+          horizontal = {
+            center = 0.5,
+            length = 0.35
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.55
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'i'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'IntelliJ IDEA'
+          app = "Messages",
+          screen = "Color LCD",
+          horizontal = {
+            center = 0.5,
+            length = 0.45
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.644
+          }
         },
         {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 's'
-            },
-            cmd = 'apps.toggle',
-            args = 'Slack'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'c'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Calendar'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'b'
-            },
-            cmd = 'apps.toggle',
-            args = 'Safari'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'c'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Calendar'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'm'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Mail'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 't'
-            },
-            cmd = 'apps.toggle',
-            args = 'Telegram'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'f'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Finder'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'u'
-            },
-            cmd = 'apps.toggle',
-            args = 'iTunes'
-        },
-        {
-            shortcut = {
-                modifiers = {'cmd', 'alt'},
-                key = 'n'
-            },
-            cmd = 'apps.launchOrFocus',
-            args = 'Notes'
-        },
-				{
-					shortcut = {
-						modifiers = {'alt'},
-						key = '`'
-					},
-					cmd = 'apps.toggle',
-					args = 'Terminal'
-				},
+          app = "Finder",
+          screen = "Color LCD",
+          horizontal = {
+            center = 0.5,
+            length = 0.45
+          },
+          vertical = {
+            center = 0.5,
+            length = 0.644
+          }
+        }
+      }
     }
+  },
+  keys = {
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "k"
+      },
+      cmd = "wnd.tall",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "j"
+      },
+      cmd = "wnd.cntr",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "h"
+      },
+      cmd = "wnd.left",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "l"
+      },
+      cmd = "wnd.right",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "f"
+      },
+      cmd = "wnd.growh",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt", "ctrl"},
+        key = "b"
+      },
+      cmd = "wnd.shrinkh",
+      args = {}
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "v"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Visual Studio Code"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "i"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "IntelliJ IDEA"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "s"
+      },
+      cmd = "apps.toggle",
+      args = "Slack"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "c"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Calendar"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "b"
+      },
+      cmd = "apps.toggle",
+      args = "Safari"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "c"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Calendar"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "m"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Mail"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "t"
+      },
+      cmd = "apps.toggle",
+      args = "Telegram"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "f"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Finder"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "u"
+      },
+      cmd = "apps.toggle",
+      args = "Cantata"
+    },
+    {
+      shortcut = {
+        modifiers = {"cmd", "alt"},
+        key = "n"
+      },
+      cmd = "apps.launchOrFocus",
+      args = "Notes"
+    },
+    {
+      shortcut = {
+        modifiers = {"alt"},
+        key = "`"
+      },
+      cmd = "apps.toggle",
+      args = "Terminal"
+    }
+  }
 }
