9bf28fca28b9b8031625d9bbaf79c5ea6f067ed3
Anton Ermolenko <aner@fastmail.com>
2019-05-08 22:42:58 +1000

    Added command to choose on of two running apps automatically.
    Updated config to automatically choose:
     - Messages if no Telegram running
     - Telegram if it is running

--- hammerspoon/.hammerspoon/apps.lua
+++ hammerspoon/.hammerspoon/apps.lua
@@ -1,20 +1,51 @@
-local apps = require 'hs.application'
-local cmds = require 'cmds'
+local apps = require "hs.application"
+local cmds = require "cmds"
+
+local function do_toggle(app_name)
+    front_app = apps.frontmostApplication()
+
+    if front_app:name() == app_name then
+        front_app:hide()
+    else
+        apps.launchOrFocus(app_name)
+    end
+end
 
 local function init()
-    cmds.register('apps.launchOrFocus', function(args)
-        apps.launchOrFocus(args)
-    end)
+    cmds.register(
+        "apps.launchOrFocus",
+        function(args)
+            apps.launchOrFocus(args)
+        end
+    )
 
-		cmds.register('apps.toggle', function(args)
-			front_app = apps.frontmostApplication()
+    cmds.register(
+        "apps.toggle",
+        function(args)
+            do_toggle(args)
+        end
+    )
 
-			if front_app:name() == args then
-				front_app:hide()
-			else
-				apps.launchOrFocus(args)
-			end
-		end)
+    cmds.register(
+        "apps.toggle.a-or-b",
+        function(args)
+            running_apps = apps.runningApplications()
+
+            app = nil
+            for _,running_app in pairs(running_apps) do
+                if running_app:name() == args[1] then
+                    app = args[1]
+                end
+            end
+
+            if app then
+                do_toggle(app)
+            else
+                do_toggle(args[2])
+            end
+                
+        end
+    )
 end
 
 return {
