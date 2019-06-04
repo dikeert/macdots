6fdda9e6e7d1791e12835e70e5ce02d352f9de6f
Anton Ermolenko <aner@fastmail.com>
2019-06-04 17:07:51 +1000

    removed mpd

--- stow.sh
+++ stow.sh
@@ -2,11 +2,9 @@
 
 mkdir -p "${HOME}/.config"
 mkdir -p "${HOME}/.local/bin"
-mkdir -p "${HOME}/.cache/mpd"
 
 stow -R bash -t "${HOME}"
 stow -R nvim -t "${HOME}"
-stow -R mpd -t "${HOME}"
 stow -R scripts -t "${HOME}/.local/bin"
 stow -R hammerspoon -t "${HOME}"
 
